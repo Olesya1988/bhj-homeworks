9d317901fd726e63d4ea7d73121d2f0fcf95edd2
Olesya Vetoshkina <our_post-office@mail.ru>
2022-12-29 10:38:40 +0300

    fourth commit variables

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,19 +1,20 @@
 const slides = Array.from(document.querySelectorAll('.slider__item'));
 const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
 const sliderArrowNext = document.querySelector('.slider__arrow_next');
-let activeSlide = document.querySelector('.slider__item_active');
-
-let slideIndex = slides.findIndex(value => value === activeSlide);
 
 sliderArrowNext.onclick = function() {
-    showSlides(slideIndex += 1);    
+    slideIndex = slides.findIndex(value => value === document.querySelector('.slider__item_active'));
+    showSlides(slideIndex += 1);         
 };
 
 sliderArrowPrev.onclick = function() {   
+    slideIndex = slides.findIndex(value => value === document.querySelector('.slider__item_active'));
     showSlides(slideIndex -= 1);
 };
 
 function showSlides(n) {
+    let slideIndex = slides.findIndex(value => value === document.querySelector('.slider__item_active'));
+    slides[slideIndex].classList.remove('slider__item_active');    
 
     if (n < 0) {
         n = (slides.length - 1);
@@ -23,6 +24,5 @@ function showSlides(n) {
         n = 0;
     }
 
-    slides[slideIndex].classList.remove('slider__item_active');
-    slides[slideIndex].classList.add('slider__item_active');
+    slides[n].classList.add('slider__item_active');
 }
