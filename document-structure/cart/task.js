8dddee233896b4054bf6e6ccfb93722c2ac32bb3
Olesya Vetoshkina <our_post-office@mail.ru>
2023-01-10 08:06:02 +0300

    document-structure commit variables

---
+++ document-structure/cart/task.js
@@ -0,0 +1,65 @@
+const minus = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
+const plus = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
+const cart = document.querySelector('.cart__products');
+const product = Array.from(document.querySelectorAll('.product'));
+
+plus.forEach((el) => {
+    el.addEventListener('click', () => {
+        const value = el.closest('.product__quantity-controls').querySelector('.product__quantity-value');
+        let amount = +value.textContent;
+        amount += 1; 
+        value.textContent = amount;
+    });
+});
+
+minus.forEach((el) => {
+    el.addEventListener('click', () => {
+        const value = el.closest('.product__quantity-controls').querySelector('.product__quantity-value');
+        let amount = +value.textContent;
+        if (amount === 1) {
+            amount; 
+        } else {
+            amount -= 1; 
+        }
+        value.textContent = amount;
+    });
+});
+
+product.forEach((el) => {
+    const productAdd = el.querySelector('.product__add');
+    productAdd.onclick = function() {
+        const value = el.querySelector('.product__quantity-value');
+        let amount = +value.textContent;
+
+        if (amount > 0) {
+            let id = el.getAttribute('data-id');
+            let src = el.querySelector('.product__image').getAttribute('src');
+
+            let products = [...cart.children];
+            const productInCard = products.find(i => i.getAttribute('data-id') === id);
+
+            if (!productInCard) {
+                cart.insertAdjacentHTML("beforeend", `
+                <div class="cart__product" data-id = ${id}>
+                <a href="#" class="cart__product-remove" style="color: red; font-size: 10px; text-decoration: none">Удалить</a>
+                <img class="cart__product-image" src=${src}>
+                <div class="cart__product-count"> ${value.textContent}</div>                    
+                </div>
+                `);
+            } else {
+                let productAmount = +productInCard.lastElementChild.textContent;
+                productInCard.lastElementChild.textContent = productAmount + amount;
+            };
+        };
+
+        const buttonRemove = Array.from(document.querySelectorAll('.cart__product-remove'));
+
+        buttonRemove.forEach((elem) => {
+            elem.onclick = function() {
+                let div = elem.closest('.cart__product');
+                div.remove();
+                return false;
+            };
+        });
+    };
+});
---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,25 @@
+const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+const tooltip = document.createElement('div');
+tooltip.classList.add('tooltip');
+
+for (let i = 0; i < hasTooltip.length; i++) {
+    hasTooltip[i].onclick = function(el) {
+        hasTooltip[i].appendChild(tooltip);
+        tooltip.textContent = hasTooltip[i].title;
+
+        if (hasTooltip[i].classList.contains('tooltip_active')) {
+            tooltip.classList.toggle('tooltip_active');
+        } else {
+            tooltip.classList.toggle('tooltip_active');
+        };
+
+        const closest = tooltip.closest('.has-tooltip');
+        const child = closest.querySelector('.tooltip');
+
+        hasTooltip[i].style.position = 'relative';
+        tooltip.style.position = 'absolute';
+        let left = closest.getBoundingClientRect().left;
+        child.style.marginLeft = left + 'px';
+        return false;
+    };
+}
