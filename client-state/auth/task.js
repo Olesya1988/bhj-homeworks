bb35408abab6942177d2ecf6f0dd4353640bedf6
Olesya Vetoshkina <our_post-office@mail.ru>
2023-01-18 15:48:01 +0300

    client-state commit variables

---
+++ client-state/auth/task.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('signin__form');
+const inputFields = Array.from(document.querySelectorAll('.control'));
+const userId = document.getElementById('user_id');
+const signin = document.querySelector('.signin');
+const welcome = document.querySelector('.welcome');
+const button = document.querySelector('.btn');
+
+form.addEventListener('submit', (event) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState === xhr.DONE) {
+            let responseText = JSON.parse(xhr.responseText);
+
+            if (responseText.success === true) {
+                inputFields.value = '';
+                localStorage.setItem('id', JSON.stringify(responseText.user_id));
+                let savedId = localStorage.getItem('id');
+                userId.textContent = savedId;
+                signin.classList.remove('signin_active');
+                welcome.classList.add('welcome_active');
+            } else {
+                alert('Неверный логин/пароль');               
+            };
+        };
+    });
+
+    button.addEventListener('click', () => {
+        let inputFieldsArray = [];
+        inputFields.forEach((elem) => {
+            inputFieldsArray.push(elem.value);
+        });
+        localStorage.setItem('key', JSON.stringify(inputFieldsArray));
+    });
+
+    window.addEventListener('load', () => {
+        let inputFieldsArray = JSON.parse(localStorage.getItem('key'));
+        inputFields.forEach((elem, i) => {
+            elem.value = inputFieldsArray[i];
+        });
+    });
+    
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    
+    const formData = new FormData(form);
+    xhr.send(formData);
+
+    event.preventDefault();
+});
